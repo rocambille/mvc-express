5c0d6f52811f99a976c810d799e97a025714ab47
romaing <rocambille@gmail.com>
2022-01-31 20:50:34 +0100

    minor fix

--- src/app.js
+++ src/app.js
@@ -10,11 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
-// load routes
+// load router
 
 const router = require("./router");
 
-app.use("/", router);
+app.use(router);
 
 // ready to export
 
