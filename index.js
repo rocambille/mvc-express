fd81bb0204cbcf19aa80ab082a0573448f3a80c9
bastienwcs <bastien.krettly@wildcodeschool.com>
2022-03-03 17:30:09 +0100

    fix migration launch

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const port = parseInt(process.env.APP_PORT ?? "5000");
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
 
     if (process.env.ENV?.toLowerCase() === "prod") {
       // eslint-disable-next-line global-require
       require("./migrate");
     }
-  } else {
-    console.log(`Server is listening on ${port}`);
   }
 });
