4f20e36779eec387df3523b71683e675fb302bc3
bastienwcs <bastien.krettly@wildcodeschool.com>
2022-03-03 17:40:30 +0100

    revert createPool on migration

--- migrate.js
+++ migrate.js
@@ -6,7 +6,7 @@ const mysql = require("mysql2/promise");
 const migrate = async () => {
   const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
-  const connection = await mysql.createPool({
+  const connection = await mysql.createConnection({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
