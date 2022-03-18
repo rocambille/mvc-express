45112d7a2cf20a7d04852ca75c5a4299928118c1
Romain Guillemot <rocambille@gmail.com>
2022-03-18 11:30:19 +0100

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -5,29 +5,29 @@ const path = require("path");
 const load = async (models) => {
   const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
-  const connection = await mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-  });
-
-  fs.readdirSync(__dirname)
-    .filter((file) => file !== "AbstractManager.js" && file !== "index.js")
-    .forEach((file) => {
-      const Manager = require(path.join(__dirname, file));
-
-      models[Manager.table] = new Manager(connection, Manager.table);
+  try {
+    const connection = await mysql.createConnection({
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
     });
+
+    fs.readdirSync(__dirname)
+      .filter((file) => file !== "AbstractManager.js" && file !== "index.js")
+      .forEach((file) => {
+        const Manager = require(path.join(__dirname, file));
+
+        models[Manager.table] = new Manager(connection, Manager.table);
+      });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const models = {};
 
-try {
-  load(models);
-} catch (err) {
-  console.log(err);
-}
+load(models);
 
 const handler = {
   get: function (obj, prop) {
